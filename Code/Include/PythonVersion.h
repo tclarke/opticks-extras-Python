da9156d39f07596eb8fce4316f216de32c06030d
kstreith <kstreith@2d91f92e-8166-0410-902e-d52292f61e86>
2010-02-05 14:29:18 +0000

    EXTRAS-26: Need access to debug DLLs for Python Interpreter. EXTRAS-27: Update the build-Python.py script to support updating the version # and add support for nightly and unofficial version # generation.
    git-svn-id: https://extras.ballforge.net/svn/extras/trunk/1.0.X/Python@349 2d91f92e-8166-0410-902e-d52292f61e86

--- Code/Include/PythonVersion.h
+++ Code/Include/PythonVersion.h
@@ -13,7 +13,7 @@
 #define PYTHON_NAME "Python"
 #define PYTHON_NAME_LONG "Python scripting"
 #define PYTHON_COPYRIGHT "Copyright © 2009, Ball Aerospace & Technologies Corp."
-#define PYTHON_VERSION_NUMBER "1.0.2rc1"
-#define PYTHON_IS_PRODUCTION_RELEASE true
+#define PYTHON_VERSION_NUMBER "1.0.0Unofficial"
+#define PYTHON_IS_PRODUCTION_RELEASE false
 
 #endif
